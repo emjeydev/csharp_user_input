e4069aef6cc78998f8e1d0e051778efe4d01ba9c
EmJeyDev <emjeydev@gmail.com>
2023-02-03 21:57:24 +0330

    add UserInput project

---
+++ TestApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            String name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            String age = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name + " You are " + age);
+
+            Console.ReadLine(); 
+        }
+    }
+}
